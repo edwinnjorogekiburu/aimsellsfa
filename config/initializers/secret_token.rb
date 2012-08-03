9496197f8fe659065f2dbb98c49b55362def703c
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-08-03 16:05:10 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aimsellsfa::Application.config.secret_token = 'eaf99d7b64a280daba47d21b9871939d3b3ae75cb2ec4f8e4869391f30320509376df95603d7bcb00ddc1185a7d6cd1591342965b3e7c7a05786221fca552a4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Aimsellsfa::Application.config.session_store :cookie_store, key: '_aimsellsfa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Aimsellsfa::Application.config.session_store :active_record_store
