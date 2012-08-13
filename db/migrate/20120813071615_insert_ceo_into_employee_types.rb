7991c721f563ddf1af3e55d28248c02ff61b397d
Edwin Njoroge Kiburu <edunjoroge@gmail.com>
2012-08-13 11:09:45 +0300

    Insert Ceo migration edit

--- db/migrate/20120813071615_insert_ceo_into_employee_types.rb
+++ db/migrate/20120813071615_insert_ceo_into_employee_types.rb
@@ -1,6 +1,6 @@
 class InsertCeoIntoEmployeeTypes < ActiveRecord::Migration
   def up
-  	@employee_type=EmployeeType.new(id: 1, name: "C.E.O", category: "Chief Executive Officer", reports_to_type_id: 0 )
+  	@employee_type=EmployeeType.new( name: "C.E.O", category: "Chief Executive Officer", reports_to_type_id: 0 )
   	@employee_type.save
   end
 
